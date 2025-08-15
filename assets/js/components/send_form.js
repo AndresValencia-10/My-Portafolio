6c02d8dc7198d5bfce4c8710642f6229dc9a4f56
AndresValencia <220809559+AndresValencia-10@users.noreply.github.com>
2025-08-14 22:16:50 -0500

    my-portafolio

---
+++ assets/js/components/send_form.js
@@ -0,0 +1,45 @@
+const formDOM = document.querySelector('#form');
+const modalDOM = document.querySelector('#modal');
+const closeModalDOM = document.querySelector('#btn_close-modal');
+
+function openModal() {
+	modalDOM.open = true;
+}
+
+function closeModal() {
+	modalDOM.open = false;
+}
+
+function getInputsData(e) {
+	e.preventDefault();
+	const infoInputs = {
+		name: document.getElementById('name').value,
+		email: document.getElementById('email').value,
+		subject: document.getElementById('subject').value,
+		message: document.getElementById('message').value,
+	};
+
+	if (
+		infoInputs.name &&
+		infoInputs.email &&
+		infoInputs.message &&
+		infoInputs.subject
+	) {
+		emailjs.send('service_s7r6wpo', 'template_pt7e79v', infoInputs).then(
+			(response) => {
+				console.log('SUCCESS!', response.status, response.text);
+				openModal();
+			},
+			(error) => {
+				console.log('FAILED...', error);
+			},
+		);
+	}
+}
+
+function sendEmail() {
+	formDOM.addEventListener('submit', getInputsData);
+}
+
+closeModalDOM.addEventListener('click', closeModal);
+export default sendEmail;
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+import dateUpdater from './helpers/date_update.js';
+import resetToHome from './helpers/reload_page.js';
+import parallax from './components/parallax.js';
+import sendEmail from './components/send_form.js';
+
+parallax();
+resetToHome();
+dateUpdater();
+sendEmail();
