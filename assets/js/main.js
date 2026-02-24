62bc4b24a67d9c1810dcbde6dd8532395e14b013
AndresValencia <220809559+AndresValencia-10@users.noreply.github.com>
2026-02-24 00:16:44 -0500

    parallax fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,8 @@ import dateUpdater from './helpers/date_update.js';
 import resetToHome from './helpers/reload_page.js';
 import parallax from './components/parallax.js';
 import sendEmail from './components/send_form.js';
-import boxicons from 'boxicons';
 
 parallax();
 resetToHome();
 dateUpdater();
 sendEmail();
-boxicons();
